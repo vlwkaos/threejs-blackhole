0ad48f768563488bdb553e384d70cbab0b9e5f53
Glitch (paint-beard) <none>
2018-11-18 11:07:45 +0000

    💋🌕 Checkpoint ./style.css:2163948/82 ./script.js:2163948/2012

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
 /* globals THREE */
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// Start with scene, camera, and renderer
+let scene
+let camera
+let renderer
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+window.onload =()=>{
+  
+  scene = new THREE.Scene();
+  
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  camera.position.x = 15;
+  camera.position.y = 16;
+  camera.position.z = 13;
+  camera.lookAt(scene.position);
+  
+  renderer = new THREE.WebGLRenderer();
+  renderer.setClearColor(0x000000, 1.0);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMapEnabled = true;
+  
+  document.body.appendChild( renderer.domElement );
+  render();
 
+}
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var geometry
+var material
+var cube 
+const sceneInit=()=>{
+  
+  geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+  
+}
 
-camera.position.z = 5;
-
-function animate() {
-	requestAnimationFrame( animate );
+const render=()=>{
+  
+  requestAnimationFrame( animate );
   
   cube.rotation.x += 0.01;
-cube.rotation.y += 0.01;
+  cube.rotation.y += 0.01;
   
 	renderer.render( scene, camera );
+  
 }
-animate();
