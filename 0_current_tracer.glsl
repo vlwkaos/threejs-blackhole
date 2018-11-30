6ef6e2b40d8b3614f9c7a543a700f2dea16878b4
Glitch (paint-beard) <none>
2018-11-30 13:03:01 +0000

    🌥💍 Checkpoint ./0_current_tracer.glsl:2163948/1886

--- 0_current_tracer.glsl
+++ 0_current_tracer.glsl
@@ -1,6 +1,6 @@
 #define PI 3.141592653589793238462643383279
 #define ROT_Y(a) mat3(0, cos(a), sin(a), 1, 0, 0, 0, sin(a), -cos(a))
-#define DEG_TO_RAD (M_PI/180.0)
+#define DEG_TO_RAD (PI/180.0)
 #define STEP 0.02
 #define NITER 10
 #define SPEED 1
@@ -10,9 +10,6 @@ const vec3 cameraDirection = vec3(0.0, 0.0, -1.0);
 const vec3 cameraUp = vec3(0.0, 1.0, 0.0);
 const float fov = 90.0;
 const float fovx = PI * fov / 360.0;
-float fovy = fovx * resolution.y/resolution.x;
-float ulen = tan(fovx);
-float vlen = tan(fovy);
 
 uniform float time;
 uniform vec2 resolution;
@@ -24,6 +21,10 @@ const float FOV_ANGLE_DEG = 90.0;
 float FOV_MULT = 1.0 / tan(DEG_TO_RAD * FOV_ANGLE_DEG*0.5);
 
 // helper functions
+vec4 blendColors(vec4 cb, vec4 ca){
+ return ca.xyz+cb.xyz*(cb.w*(1-ca.w)); 
+}
+
 vec2 squareFrame(vec2 screenSize){
   
   vec2 position = 2.0 * (gl_FragCoord.xy / screenSize.xy) - 1.0;
@@ -32,46 +33,31 @@ vec2 squareFrame(vec2 screenSize){
 }
 
 vec2 sphereMap(vec3 p){
-  return vec2(atan(p.x,p.y)/M_PI*0.5+0.5, asin(p.z)/M_PI+0.5);
+  return vec2(atan(p.x,p.y)/PI*0.5+0.5, asin(p.z)/PI+0.5);
 }
 
 
-// Ray represents a ray of light's origin and direction
-struct Ray {
-  vec3 origin; // Origin
-  vec3 direction; // Direction
-};
-
-
-vec3 lorentz_velocity_transformation(vec3 moving_v, vec3 frame_v) {
-    float v = length(frame_v);
-    if (v > 0.0) {
-        vec3 v_axis = -frame_v / v;
-        float gamma = 1.0/sqrt(1.0 - v*v);
-
-        float moving_par = dot(moving_v, v_axis);
-        vec3 moving_perp = moving_v - v_axis*moving_par;
-
-        float denom = 1.0 + v*moving_par;
-        return (v_axis*(moving_par+v)+moving_perp/gamma)/denom;
-    }
-    return moving_v;
-}
-
-vec3 leapFrog(vec3 point, vec3 velocity){
- 
-  
-  return point;
-}
 
 void main()	{
-  vec2 p = squareFrame(resolution);
-  vec3 pixel_pos = vec3(p.xy, 0.);
-    
-  // cam position
-  vec3 cam_pos = vec3(0,0,-5);
+  // camera variables 
+  float fovy = fovx * resolution.y/resolution.x;
+  float ulen = tan(fovx);
+  float vlen = tan(fovy);
+  
+  
+  vec2 uv = squareFrame(resolution);
+  // generate ray
+  vec3 nright = normalize(cross(cameraUp, cameraDirection));
+  vec3 pixelPos = cameraPosition + cameraDirection +
+                 nright*uv.x*ulen + cameraUp*uv.y*vlen;
+  vec3 rayDirection = normalize(pixelPos - cameraPosition);
 
-  //leap frog
+  // initial color
+  vec4 color = vec4(0.5,1.0,0.5,0.4);
+  vec4 colorb = vec4(0.3,0.1,0.1,0.5);
+  
+  /*
+  // geodesic by leapfrog integration
   vec3 point = pixel_pos;
   vec3 velocity = normalize(cam_pos - point);
   
@@ -86,9 +72,9 @@ void main()	{
     vec3 accel = -1.5 * h2 * point / pow(dot(point,point),5.0);
     velocity += accel * STEP;    
   }
-  velocity = normalize(point - old_point); 
-  vec2 tex_coord = sphereMap(velocity * BG_COORDS);  
-  gl_FragColor = texture2D(bg_texture, tex_coord);
+  */
+  
+  gl_FragColor = blendColor(color, colorb);
   //color intesection
   
 }
