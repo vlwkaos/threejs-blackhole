82f97b09a901c1ce9a7599aa9a2767b0b88d1a56
Glitch (three-js-blackhole) <none>
2018-12-19 11:40:30 +0000

    🏖🌿 Checkpoint ./0_current_tracer.glsl:2163948/1043

--- 0_current_tracer.glsl
+++ 0_current_tracer.glsl
@@ -58,11 +58,32 @@ vec3 temp_to_color(float temp_kelvin){
   vec3 color;
   // 1k ~ 40k rescale by deviding 100
   temp_kelvin = clamp(temp_kelvin, 1000.0, 40000.0) / 100.0;
-  if (temp_kelvin <= 66){
-    color.r = 255;
+  if (temp_kelvin <= 66.0){
+    color.r = 255.0;
     color.g = temp_kelvin;
-    color.g = 99.4708025861 * log
+    color.g = 99.4708025861 * log(color.g) - 161.1195681661;
+    if (color.g < 0.0) color.g = 0.0;
+    if (color.g > 255.0)  color.g = 255.0;
+  } else {
+    color.r = temp_kelvin - 60.0;
+    color.r = 329.698727446 * pow(color.r, -0.1332047592);
+    if (color.r < 0.0) color.r = 0.0;
+    if (color.g > 255.0) color.r = 255.0;
+    color.g = temp_kelvin - 60.0;
+    color.g = 288.1221695283 * pow(color.g, -0.0755148492);
+    if (color.g < 0.0) color.g = 0.0;
+    if (color.g > 255.0)  color.g = 255.0;  
   }
+  if (temp_kelvin >= 66.0){
+    color.b = 0.0;
+  } else {
+    color.b = temp_kelvin - 10.0;
+    color.b = 138.5177312231 * log(color.b) - 305.0447927307;
+    if (color.b < 0.0) color.b = 0.0;
+    if (color.b > 255.0) color.b = 255.0;
+  }
+  color /= 255.0;
+  return color;
 }
 
 void main()	{
