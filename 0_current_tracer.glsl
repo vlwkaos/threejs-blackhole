1badfa517c8965c4c5844b7ac69e9796cedcbdc5
Glitch (three-js-blackhole) <none>
2018-12-24 06:09:09 +0000

    🤑⛲️ Checkpoint ./0_current_tracer.glsl:2163948/67

--- 0_current_tracer.glsl
+++ 0_current_tracer.glsl
@@ -2,8 +2,8 @@
 #define DEG_TO_RAD (PI/180.0)
 #define ROT_Y(a) mat3(1, 0, 0, 0, cos(a), sin(a), 0, -sin(a), cos(a))
 #define ROT_Z(a) mat3(cos(a), -sin(a), 0, sin(a), cos(a), 0, 0, 0, 1)
-#define STEP 0.1
-#define NSTEPS 100
+#define STEP 0.2
+#define NSTEPS 50
 #define SPEED 1
 
 
