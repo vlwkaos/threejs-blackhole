88ad4d663e5d5a7ae5e3fb095bedc8c1b10645e9
Glitch (three-js-blackhole) <none>
2018-12-25 02:06:52 +0000

    🎊🎌 Checkpoint ./0_current_tracer.glsl:2163948/129 ./main.js:2163948/4

--- main.js
+++ main.js
@@ -53,8 +53,8 @@ const init = ()=>{
   loadTexture('bg1','https://raw.githubusercontent.com/oseiskar/black-hole/master/img/milkyway.jpg', THREE.NearestFilter)
   loadTexture('bg2','https://cdn-images-1.medium.com/max/2000/1*i53XJF3x04oq3BUJHy4TQQ.png', THREE.NearestFilter)
   loadTexture('star','https://raw.githubusercontent.com/oseiskar/black-hole/master/img/stars.png', THREE.LinearFilter)
-  //loadTexture('disk','https://raw.githubusercontent.com/oseiskar/black-hole/master/img/accretion-disk.png', THREE.LinearFilter)
-  loadTexture('disk','https://raw.githubusercontent.com/rantonels/starless/master/textures/adisk.jpg', THREE.LinearFilter)
+  loadTexture('disk','https://raw.githubusercontent.com/oseiskar/black-hole/master/img/accretion-disk.png', THREE.LinearFilter)
+  //loadTexture('disk','https://raw.githubusercontent.com/rantonels/starless/master/textures/adisk.jpg', THREE.LinearFilter)
   // screen frame
   uniforms = {
 		time: { type: "f", value: 1.0 },
