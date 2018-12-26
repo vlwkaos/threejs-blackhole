209fdba42f35cdc32a5d2bcbadd93beab22c7485
Glitch (three-js-blackhole) <none>
2018-12-26 15:24:08 +0000

    😎🎓 Checkpoint ./main.js:2163948/448

--- main.js
+++ main.js
@@ -171,16 +171,14 @@ const addControlGUI = ()=>{
       getImageData = true;
       render()
       renderer.domElement.toBlob(function(blob) {
-           let URLObj = window.URL || window.webkitURL;
-            let a = document.createElement("a");  
+        let URLObj = window.URL || window.webkitURL;
+        let a = document.createElement("a");  
         a.href = URLObj.createObjectURL(blob);
         a.download = "untitled.png";
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
       });
-      let image = renderer.domElement.toDataURL("image/png").replace("image/png", "image/octet-stream");  
-      window.location.href=image;
     }
     
   }
